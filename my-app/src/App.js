0c2cec909cc414a3e1d7ad5ae8304a6aba7163f3
NathanBENAIS <96385008+NathanBENAIS@users.noreply.github.com>
2021-12-21 20:02:04 +0100

    add

---
+++ my-app/src/App.js
@@ -0,0 +1,54 @@
+import React, { Fragment, useState, useEffect } from 'react'
+import Slideshow from './Slide';
+import './App.css'
+import axios from 'axios';
+
+function App() {
+
+  const [clicked, plusone] = useState(0);
+
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'https://jsonplaceholder.typicode.com/posts',
+
+      );
+      setData(result.data);
+
+    };
+    fetchData();
+  }, []) // pour pas qu'il fasse l'appel de api a l'infini.
+
+  return (
+    <div>
+      <h2 className="App">Voici mes differents contenu<hr /></h2>
+      <div className="App"><Slideshow /></div>
+
+      <div><hr className='hrv' /></div>
+
+      <div className="App">
+        <p>Nombre de fois que tu as cliqué : {clicked}</p>
+        <button className="AppBouton"
+          onClick={() => {
+            plusone(clicked + 1);
+          }}>Clique</button>
+      </div>
+      <br/>
+      <div><hr className='hrv' /></div>
+
+      <Fragment>
+        <ul>
+          {data.map(item => (
+            <li key={item.id}>
+
+              <p>{item.title}</p>
+            </li>
+          ))}
+        </ul>
+      </Fragment>
+    </div>
+  );
+}
+
+export default App;
